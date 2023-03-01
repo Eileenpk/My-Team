14fed2d9887bcddd0f4c0c7da2f6068bc74a4c4f
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-03-01 17:17:36 -0500

    finish: Contact.js header tablet CSS

--- pages/Contact.js
+++ pages/Contact.js
@@ -3,12 +3,24 @@ import ContactBenefitCard from './components/ContactBenefitCard';
 export default function Contact() {
   return (
     <>
-      <header className={`pt-20 pb-14  ${styles.header}`}>
+      <header className={`pt-20 pb-14 md:flex md:flex-col md:items-center ${styles.header}`}>
         <h1 className={`mb-4 ${styles.heading}`}>Contact</h1>
         <h2 className={`mb-10 ${styles.headerText}`}>Ask us about</h2>
-        <ContactBenefitCard image='./images/icon-person.svg' info='The quality of our talent network' />
-        <ContactBenefitCard image='./images/icon-cog.svg' info='Usage & implementation of our software' />
-        <ContactBenefitCard image='./images/icon-chart.svg' info='How we help drive innovation' />
+        <div>
+          <ContactBenefitCard
+            image="./images/icon-person.svg"
+            info="The quality of our talent network"
+            className={styles.card}
+          />
+          <ContactBenefitCard
+            image="./images/icon-cog.svg"
+            info="Usage & implementation of our software"
+          />
+          <ContactBenefitCard
+            image="./images/icon-chart.svg"
+            info="How we help drive innovation"
+          />
+        </div>
       </header>
     </>
   );
