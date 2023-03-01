149632d1a4d5c711891002019bc221de06f0ab9e
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-03-01 11:41:01 -0500

    fix:font import, finish:getStarted.js CSS for tablet and desktop

--- pages/components/GetStarted.js
+++ pages/components/GetStarted.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function GetStarted() {
   return (
-    <section className={` py-20 ${styles.getStarted}`}>
+    <section className={` py-20 md:flex ${styles.getStarted}`}>
       <h2 className={`mb-6 ${styles.heading}`}>Ready to get started?</h2>
       <Link href="/Contact" className={`button m-auto mb-6 ${styles.button}`}>
         contact us
