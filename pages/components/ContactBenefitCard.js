d2a68e9dbea880e1ab9541ea005b1c7af72630ea
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-03-09 13:47:49 -0500

    fix:alt att for ContactBenefitCard.js

--- pages/components/ContactBenefitCard.js
+++ pages/components/ContactBenefitCard.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function ContactBenefitCard ({image, info}) {
     return (
         <section className={`flex items-center justify-start mb-8 px-[6.5%] xl:px-0`}>
-            <Image src={image} width={72} height={72} className={`mr-6`}/>
+            <Image src={image} width={72} height={72} className={`mr-6`} alt=''/>
             <h3>{info}</h3>
         </section>
     )
